a47187e24ba7c6603cf3612dadeed7a789f43b89
Michael Symmes <msymmes@gmail.com>
2015-04-27 22:49:08 -0400

    Added sinon and some stub tests

--- test.js
+++ test.js
@@ -1,8 +1,45 @@
 var expect = chai.expect;
 
 describe('Test', function() {
-  it ("should be able to run tests", function() {
-    var testArray = [1, 2, 3, 4, 'five'];
-    expect(testArray.length).to.equal(5);
+
+
+  describe("Basic expectation: ", function() {
+    it ("should be able to run tests", function() {
+      var testArray = [1, 2, 3, 4, 'five'];
+      expect(testArray.length).to.equal(5);
+    });
+  });
+
+  describe("Sinon stubs", function() {
+    var sandbox;
+
+    beforeEach(function() {
+      // create a sandbox
+      sandbox = sinon.sandbox.create();
+
+      // stub some console methods
+      sandbox.stub(window.console, "log");
+      sandbox.stub(window.console, "error");
+    });
+
+    afterEach(function() {
+      // restore the environment as it was before
+      sandbox.restore();
+    });
+    it("should log errors", function() {
+      console.error("Man thats a nasty error!");
+
+      sinon.assert.notCalled(console.log);
+      sinon.assert.calledOnce(console.error);
+      sinon.assert.calledWithExactly(console.error, "Man thats a nasty error!")
+    });
+
+    it("should log normally", function() {
+      console.log("Nice log to console bro!");
+
+      sinon.assert.notCalled(console.error);
+      sinon.assert.calledOnce(console.log);
+      sinon.assert.calledWithExactly(console.log, "Nice log to console bro!")
+    });
   });
 });
